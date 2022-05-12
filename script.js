974d31c99fe4c1a9709e6b7c73eddf776c8aab9e
Sergey Barchshevskiy <sergey.barchshevskiy@ibm.com>
2022-05-11 20:42:09 -0400

    designes the form and bring computing functionalities

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function compute()
+const compute = (rate, principal, term) =>
 {
-    p = document.getElementById("principal").value;
-    
+    let interest = principal * (rate/100) * term;
+
+    document.getElementById('rate').value = rate; 
+    document.getElementById("principal").value = principal;
+
+    document.getElementById("term").value = term;
+    document.getElementById('interest').innerHTML = interest;
+
+    return interest;
 }
-        
+
