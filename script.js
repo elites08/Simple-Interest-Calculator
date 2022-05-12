ea93f091731ba70c4be7ef3418d140b18efe75ec
Sergey Barchshevskiy <sergey.barchshevskiy@ibm.com>
2022-05-11 23:07:53 -0400

    corrected errors and colors

--- script.js
+++ script.js
@@ -7,20 +7,24 @@ const interestSlider = (val) => {
 
 const computeInterest = () => {
     
-    let principal = parseInt(document.getElementById("principal").value);
+    let principal = document.getElementById("principal").value;
 
     document.getElementById("interest-range").value = rate;
     let term = document.getElementById("term").value;
     let result = principal * (rate/100) * term;
 
-    if(rate === undefined){
-        alert("Please select the interest rate")
-        window.location.reload();
-    } else {
-        return document.getElementById('interest').innerHTML = result;
-
+    if(principal <= 0 ) {
+        alert("Please enter a positive value for principal")
+    }
+    if(term <= 0) {
+        alert("Please enter a positive term")
     }
 
+    if(rate === undefined){
+        alert("Please select the interest rate")
+    } else {
+        return document.getElementById('interest').innerHTML = result;
+    }
 
 }
 
